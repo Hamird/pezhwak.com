6c2c657145f98f64008b87d22cfeb6a05b4e68bd
sina <sina.sabbagh@gmail.com>
2015-09-17 02:39:18 +0430

    Add basic coloring - buggy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('#pagepiling').pagepiling();
+  $('#pagepiling').pagepiling({
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    menu: '#menu',
+    anchors: ['first', 'second', 'third', 'fourth'],
+    css3: false
+  });
 });
